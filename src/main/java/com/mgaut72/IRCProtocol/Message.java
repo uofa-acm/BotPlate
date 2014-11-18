cfb983659e35f233a0c0aeeb21a52ee3c9abde87
Matt Gautreau <mgaut72@gmail.com>
2014-11-18 10:32:21 -0700

    add IRC message class template

---
+++ src/main/java/com/mgaut72/IRCProtocol/Message.java
@@ -0,0 +1,12 @@
+package com.mgaut72.IRCProtocol;
+
+class Message {
+
+    private String prefix;
+    private String command;
+    private List<String> parameters;
+
+    public Message(String rawMessage){
+
+    }
+}
